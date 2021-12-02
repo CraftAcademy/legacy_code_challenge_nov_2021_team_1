171795b6806a6d5a970989e132145ddd99515bf7
gsim1903 <gavin@internetbusker.co>
2021-12-02 05:59:30 +0100

    update gemfile, create migrate DB

--- spec/models/analysis_spec.rb
+++ spec/models/analysis_spec.rb
@@ -3,4 +3,9 @@ RSpec.describe Analysis, type: :model do
   it { is_expected.to have_db_column :resource }
   it { is_expected.to have_db_column :request_ip }
   it { is_expected.to have_db_column :results }
+
+  it 'is expected to have a valid factory' do
+    expect(create(:analsis)).to be_valid
+    end
+    
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,20 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'spec_helper'
 
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+module ResponseJSON
+  def response_json
+    JSON.parse(response.body)
+  end
+end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
