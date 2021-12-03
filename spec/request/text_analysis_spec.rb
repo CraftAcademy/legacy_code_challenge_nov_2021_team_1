0b22ce78fccc904d3c6ed4b85e8a18daa32fc65f
DefyCab <love.bellinder@gmail.com>
2021-12-03 11:06:23 +0100

    refactor analysis test to separate it blocks

--- spec/request/text_analysis_spec.rb
+++ spec/request/text_analysis_spec.rb
@@ -14,19 +14,14 @@ RSpec.describe Api::AnalysesController, type: :request do
     it 'is expected to return the requested resource' do
       expect(response_json['resource']).to eq 'This is awesome'
     end
-    it 'is expected to return the results of the analysis' do
+    it 'is expected to show status clean' do
+      expect(eval_json[0]['tag_name']).to eq 'clean'
+    end
+    it 'is expected to show confidence in the analysis' do
       expect(eval_json[0]['confidence']).to eq 0.844
-      expect(eval_json[0]['tag_name']).to eq "clean"
-      expect(eval_json[0]['tag_id']).to eq 57725628
-      end
+    end
+    it 'is expected to show the tag_id' do
+      expect(eval_json[0]['tag_id']).to eq 57_725_628
     end
   end
-          
-
-        
-        
-            
-       
-        
-
-
+end
